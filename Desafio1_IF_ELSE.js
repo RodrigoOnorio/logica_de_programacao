7aafc8380a99587ce94c769e3f26a3f07f41dbd1
Rodrigo <akatroro@gmail.com>
2025-05-29 11:00:42 -0300

    logica_de_programacao

---
+++ Desafio1_IF_ELSE.js
@@ -0,0 +1,195 @@
+let correta = 0
+let errada = 0
+
+// Pergunta 1
+let enunciado1 = ("Qual é o maior país do mundo em território?")
+console.log(enunciado1)
+console.log("A China")
+console.log("B Canadá")
+console.log("C Estados Unidos")
+console.log("D Rússia")
+console.log("E Brasil")
+
+let pergunta1 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta1 === "d" || pergunta1 === "D") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é Rússia!")
+    errada++
+}
+
+// Pergunta 2
+let enunciado2 = ("Quem pintou a Mona Lisa?")
+console.log(enunciado2)
+console.log("A Van Gogh")
+console.log("B Leonardo da Vinci")
+console.log("C Picasso")
+console.log("D Salvador Dali")
+console.log("E Claude Monet")
+let pergunta2 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta2 === "B" || pergunta2 === "b") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é Leonardo da Vinci!")
+    errada++
+}
+
+// Pergunta 3
+let enunciado3 = ("Quantos ossos tem o corpo humano adulto?")
+console.log(enunciado3)
+console.log("A 206")
+console.log("B 276")
+console.log("C 329")
+console.log("D 409")
+console.log("E 127")
+let pergunta3 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta3 === "A" || pergunta3 === "a") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é 206!")
+    errada++
+}
+
+// Pergunta 4
+let enunciado4 = ("Qual é o nome do rio que corta a cidade de Londres?")
+console.log(enunciado4)
+console.log("A Rio Senna")
+console.log("B Rio Amazonas")
+console.log("C Rio Danúbio")
+console.log("D Rio Tâmisa")
+console.log("E Rio Mississipi")
+let pergunta4 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta4 === "D" || pergunta4 === "d") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é o Rio Tâmisa!")
+    errada++
+}
+
+// Pergunta 5
+let enunciado5 = ("Quem foi o primeiro presidente do Brasil?")
+console.log(enunciado5)
+console.log("A Lula")
+console.log("B Juscelino Kubitschek")
+console.log("C Deodoro Fonseca")
+console.log("D Getúlio Vargas")
+console.log("E Dom Pedro II")
+let pergunta5 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta5 === "C" || pergunta5 === "c") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é Deodoro Fonseca!")
+    errada++
+}
+
+// Pergunta 6
+let enunciado6 = ("Qual é o elemento químico representado pelo símbolo 'Au' ?")
+console.log(enunciado6)
+console.log("A Alúminio")
+console.log("B Ferro")
+console.log("C Ouro")
+console.log("D Prata")
+console.log("E Cobre")
+let pergunta6 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta6 === "C" || pergunta6 === "c") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é Ouro!")
+    errada++
+}
+
+// Pergunta 7
+let enunciado7 = ("Quantos lados tem um undecágono?")
+console.log(enunciado7)
+console.log("A 11")
+console.log("B 18")
+console.log("C 21")
+console.log("D 7")
+console.log("E 9")
+let pergunta7 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta7 === "A" || pergunta7 === "a") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é 11!")
+    errada++
+}
+
+// Pergunta 8
+let enunciado8 = ("Qual é o maior mamífero do mundo?")
+console.log(enunciado8)
+console.log("A Elefante-Africano")
+console.log("B Hipopótamo Angolano")
+console.log("C Girafa Asiática")
+console.log("D Urso Polar")
+console.log("E Baleia Azul")
+let pergunta8 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta8 === "E" || pergunta8 === "e") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é a Baleia Azul!")
+    errada++
+}
+
+// Pergunta 9
+let enunciado9 = ("Quem escreveu 'Dom Quixote'?")
+console.log(enunciado9)
+console.log("A William Shakespeare")
+console.log("B Miguel de Cervantes")
+console.log("C Machado de Assis")
+console.log("D J. R. R. Tolkien")
+console.log("E Dante Alighieri")
+let pergunta9 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta9 === "B" || pergunta9 === "b") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é Miguel de Cervantes!")
+    errada++
+}
+
+// Pergunta 10
+let enunciado10 = ("Em que ano ocorreu a Independência do Brasil?")
+console.log(enunciado10)
+console.log("A 1500")
+console.log("B 1914")
+console.log("C 2002")
+console.log("D 1889")
+console.log("E 1822")
+let pergunta10 = prompt("Digite a sua resposta (A,B,C,D,E): ")
+if (pergunta10 === "E" || pergunta10 === "e") {
+    console.log("Resposta Correta!")
+    correta++
+} else {
+    console.log("Resposta Incorreta!")
+    console.log("A resposta correta é 1822!")
+    errada++
+}
+
+// Exibir Resultado
+console.log("\nResumo final: ")
+console.log("Respostas corretas: " + correta)
+console.log("Respostas incorretas: " + errada)
+
+// Teste de Aprovação
+if (correta >= 7 && errada <= 3) {
+    console.log("Parabéns, você passou no quiz!!!")
+} else {
+    console.log("Infelizmente você não passou no quiz : ( \n Tente novamente para resultados melhores...")
+}
